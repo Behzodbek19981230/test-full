060eed3bdab030909b798ee644f7e6aa30c563fd
b_rasulov <fattoyeva_n@nuu.uz>
2026-06-25 17:00:55 +0500

    Add Yandex Metrika integration for analytics tracking

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -92,6 +92,8 @@ export const metadata: Metadata = {
 	},
 };
 
+const YM_ID = process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID;
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	const jsonLd = {
 		'@context': 'https://schema.org',
@@ -128,8 +130,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 					rel='stylesheet'
 				/>
 				<script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+					{YM_ID && (
+						<script
+							dangerouslySetInnerHTML={{
+								__html: `
+									(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+									m[i].l=1*new Date();
+									for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return;}
+									k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+									(window,document,"script","https://mc.yandex.ru/metrika/tag.js?id=${YM_ID}","ym");
+									ym(${YM_ID},"init",{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
+								`,
+							}}
+						/>
+					)}
 			</head>
 			<body>
+				{YM_ID && (
+					<noscript>
+						<div>
+							<img src={`https://mc.yandex.ru/watch/${YM_ID}`} style={{ position: 'absolute', left: '-9999px' }} alt='' />
+						</div>
+					</noscript>
+				)}
 				<Providers>{children}</Providers>
 			</body>
 		</html>
